18489e04572d069110fa0fe5b36c0ed098f6cbfb
StevenWuTG <stevenwu91@icloud.com>
2020-11-02 11:55:59 -0500

    mvp

---
+++ app/controllers/artist_instruments_controller.rb
@@ -0,0 +1,24 @@
+class ArtistInstrumentsController < ApplicationController
+
+    def new
+        @artist_instrument = ArtistInstrument.new
+    end
+
+    def create
+        @artist_instrument = ArtistInstrument.create(new_params)
+        if @artist_instrument.valid?
+            redirect_to artist_path(@artist_instrument.artist_id)
+        else
+            flash[:errors] = @artist_instrument.errors.full_messages
+            redirect_to new_artist_instrument_path
+        end
+    end
+
+    private
+
+    def new_params
+        params.require(:artist_instrument).permit(:artist_id, :instrument_id)
+    end
+
+
+end
